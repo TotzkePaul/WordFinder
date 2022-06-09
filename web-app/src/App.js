542bc43544e749cb5078fe98087e4029db613655
Paul Totzke <7072k3@gmail.com>
2022-06-09 12:59:32 -0500

    add web app

---
+++ web-app/src/App.js
@@ -0,0 +1,129 @@
+
+import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import file from './words.txt';
+
+
+
+function WordListItem(props) {
+  return <li>{ props.text }</li>;
+}
+
+// Create a row of character inputs equal to the wordlength
+function WordRow(props) {
+  const row = [];
+  for (let i = 0; i < props.wordLength; i++) {
+    row.push(
+      <label for={'chk' +i}>
+        
+        <input className='square' type="text" key={i} />
+        <br/>
+        <input id={'chk' +i} key={i} type='checkbox' />
+      </label>
+    
+    );
+  }
+  return <div>{row}</div>;
+}
+
+function App() {
+
+  const [words, setWords] = useState('');
+  const [wordlength, setWordlength] = useState(5);
+  const [startFilter, setStartFilter] = useState('');
+  const [endFilter, setEndFilter] = useState('');
+  const [containsFilter, setContainsFilter] = useState('');
+  const [excludingFilter, setExcludingFilter] = useState('');
+
+  const filetext = async file => {
+      const response = await fetch(file)
+      const text = await response.text()
+      return text;
+  }
+
+  if(words === '') {
+    const text = filetext(file).then(text =>  setWords(text));
+  }
+  
+const exclude = (word, excluded) => {
+      for(let i = 0; i < excluded.length; i++) {
+        if(word.includes(excluded[i]) ) {
+          return true;
+        }
+      }
+      return false;
+  }
+
+  const include = (word, include) => {
+    for(let i = 0; i < include.length; i++) {
+      if(!word.includes(include[i]) ) {
+        return false;
+      }
+    }
+    return true;
+}
+  // Get words of wordlength and does not include any excluded character in excludeFilter
+  let wordList = words.split('\n');
+
+  const results = wordList.filter(word => word.length === wordlength)
+  .filter(word => word.startsWith(startFilter))
+  .filter(word => word.endsWith(endFilter))
+  .filter(word => containsFilter === '' || include (word, containsFilter))
+  .filter(word => excludingFilter === '' || !exclude (word, excludingFilter));
+
+
+
+  return (
+    <div className="App">
+
+          <form>
+            <label>Enter Length:
+              <input
+                type="text" 
+                defaultValue={wordlength}
+                onChange={(e) => setWordlength(parseInt(e.target.value))}
+              />
+            </label>
+            <label>Starts With:
+              <input
+                type="text" 
+                defaultValue={startFilter}
+                onChange={(e) => setStartFilter((e.target.value))}
+              />
+            </label>
+            <label>Ends With:
+              <input
+
+                type="text"
+                defaultValue={endFilter}
+                onChange={(e) => setEndFilter((e.target.value))}
+              />
+            </label>
+            <label>Contains:
+              <input
+                type="text"
+
+                defaultValue={containsFilter}
+                onChange={(e) => setContainsFilter((e.target.value))}
+              />
+            </label>
+            <label>Excluding:
+              <input
+                type="text"
+                defaultValue={excludingFilter}
+                onChange={(e) => setExcludingFilter((e.target.value))}
+              />
+            </label>
+            <WordRow wordLength={wordlength} />
+          </form>
+          <div style={{maxHeight: 400, overflow: 'auto'}}>
+          <ul>
+            {results.map((result) => <WordListItem text={result} />)}
+          </ul>
+          </div>
+    </div>
+  );
+}
+
+export default App;
