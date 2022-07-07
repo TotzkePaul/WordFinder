ca0d9974796650c6c0f9872fa65de4948e9a3fc1
Travis Nordrum <Tnord261@gmail.com>
2022-07-07 11:30:06 -0500

    Fized formatting issue and applied fix to allow multiple lines of input boxes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -6,37 +6,95 @@ import file from './words.txt';
 
 
 
+
+
 function WordListItem(props) {
-  return <li>{ props.text }</li>;
+  return <li key={props.text}>{ props.text }</li>;
 }
 
-// Create a row of character inputs equal to the wordlength
-function WordRow(props) {
-  const row = [];
-  for (let i = 1; i < (props.wordLength+1); i++) {  
-    row.push(
-//testing htmlFor vs for
-      <label htmlFor={'chk' +i}> 
-              <input className='square' type="text" pattern="[a-z]{1}" key={i} maxLength="1"/>                
-                
-        <br/>
-        <input id={'chk' +i} key={((i*100))} type='checkbox' /> 
 
-        
-      </label>
-          
-    );
-  }   
-
-
- 
-  return <div> <boxinput> {row} </boxinput> </div>; 
-}
 
 function App() {
 
-  const [words, setWords] = useState(''); 
   const [wordlength, setWordlength] = useState(5);
+
+  const [inputFields, setInputFields] = useState(
+    [
+      [
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false}
+      ],
+      [
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false}
+      ],
+      [
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false}
+      ],
+      [
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false}
+      ],
+      [
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false},
+        {letter: '', checked: false}
+      ]
+
+
+    ]
+    
+    );
+
+  const handleFormChange = (idx, index, event) => {
+    let data = [...inputFields]
+    data[idx][index][event.target.name] = event.target.value;
+    setInputFields(data);
+  }
+
+  //function WordLetter(props) {
+    //return (<label > 
+        //<input className='square' type="text" pattern="[a-z]{1}" key={props.keyId} maxLength="1"/>     
+        //<br/>
+        //<input type='checkbox' /> 
+      //</label>);
+  //}
+  // Create a row of character inputs equal to the wordlength
+  function WordRow(props) {
+    //const row = [...Array(props.wordLength).keys()].map(a=>a+1);
+    const idx = props.idx;
+    const row = props.inputs.map((input, index) => {
+      return (
+        <div key={index}>
+          <label > 
+            <input name='letter' className='square' type="text" pattern="[a-z]{1}" value={input.letter} maxLength="1" onChange={event => handleFormChange(idx, index, event)}/>     
+            <br/>
+            <input name='checked' type='checkbox' value={input.checked} onChange={event => handleFormChange(idx, index, event)} /> 
+          </label>
+        </div>
+      )
+    });
+   
+    return <div className='boxinput'>  {row} </div>; 
+  }
+
+  const [words, setWords] = useState(''); 
+  
   const [startFilter, setStartFilter] = useState('');
   const [endFilter, setEndFilter] = useState('');
   const [containsFilter, setContainsFilter] = useState('');
@@ -55,10 +113,10 @@ function App() {
 const exclude = (word, excluded) => {
       for(let i = 0; i < excluded.length; i++) {
         if(word.includes(excluded[i]) ) {
-          return true;
+          return false;
         }
       }
-      return false;
+      return true;
   }
 
   const include = (word, include) => {
@@ -68,17 +126,32 @@ const exclude = (word, excluded) => {
       }
     }
     return true;
-}
+  }
+
+  const wordleLogic = (word, include) => {
+    for(let i = 0; i < include.length; i++) {
+      if(include[i].letter == '') {
+        continue;
+      } else if( include[i].letter != word[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   // Get words of wordlength and does not include any excluded character in excludeFilter
-  let wordList = words.split('\r\n');
+  let wordList = words.replaceAll('\r\n','\n').split('\n');
 
   const results = wordList.filter(word => word.length === wordlength)
   .filter(word => word.startsWith(startFilter))
   .filter(word => word.endsWith(endFilter))
+  .filter(word => wordleLogic(word, inputFields[0] ))
+  .filter(word => wordleLogic(word, inputFields[1] ))
+  .filter(word => wordleLogic(word, inputFields[2] ))
+  .filter(word => wordleLogic(word, inputFields[3] ))
+  .filter(word => wordleLogic(word, inputFields[4] ))
   .filter(word => containsFilter === '' || include (word, containsFilter))
-  .filter(word => excludingFilter === '' || !exclude (word, excludingFilter)); //seems to be a bug regarding exlude
-
-
+  .filter(word => excludingFilter === '' || exclude (word, excludingFilter)); //seems to be a bug regarding exlude
 
   return (
     <div className="App">
@@ -95,7 +168,7 @@ const exclude = (word, excluded) => {
               <input
                 type="text" 
                 defaultValue={startFilter}
-                onChange={(e) => setStartFilter((e.target.value.toLowerCase()))}
+                onChange={(e) => setStartFilter((e.target.value?.toLowerCase()))}
               />
             </label>
             <label>Ends With:
@@ -103,7 +176,7 @@ const exclude = (word, excluded) => {
 
                 type="text"
                 defaultValue={endFilter}
-                onChange={(e) => setEndFilter((e.target.value.toLowerCase()))}
+                onChange={(e) => setEndFilter((e.target.value?.toLowerCase()))}
               />
             </label>
             <label>Contains:
@@ -111,22 +184,26 @@ const exclude = (word, excluded) => {
                 type="text"
 
                 defaultValue={containsFilter}
-                onChange={(e) => setContainsFilter((e.target.value.toLowerCase()))}
+                onChange={(e) => setContainsFilter((e.target.value?.toLowerCase()))}
               />
             </label>
             <label>Excluding:
               <input
                 type="text"
                 defaultValue={excludingFilter}
-                onChange={(e) => setExcludingFilter((e.target.valueString.toLowerCase()))}
+                onChange={(e) => setExcludingFilter((e.target.value?.toLowerCase()))}
               />
             </label>
            
-              <WordRow wordLength={wordlength} />   
+              <WordRow idx={0} inputs={inputFields[0]} />   
+              <WordRow idx={1} inputs={inputFields[1]} />   
+              <WordRow idx={2} inputs={inputFields[2]} />   
+              <WordRow idx={3} inputs={inputFields[3]} />   
+              <WordRow idx={4} inputs={inputFields[4]} />   
           </form>
           <div style={{maxHeight: 400, overflow: 'auto'}}>
           <ul>
-            {results.map((result) => <WordListItem text={result} />)}
+            {results.map((result) => <WordListItem key={result} text={result} />)}
           </ul>
           </div>
     </div>
@@ -135,3 +212,6 @@ const exclude = (word, excluded) => {
 }
 
 export default App;
+
+
+
